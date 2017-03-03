b68487f0771476866cfe395bb259218cbcadbc4e
samblackmore <samblackmore07@gmail.com>
2017-03-02 22:04:07 -0500

    adding rules for numbers and operators

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var buttons = [];
-var initialMessage = document.getElementById('display').textContent;
+var operators = ['+','-','*','/'];
+var numbers = [0,1,2,3,4,5,6,7,8,9].map(function(n) {return String(n)});
 
 // ASCII codes for all the buttons on our calculator
 // plus a pesky comma
@@ -19,15 +20,40 @@ buttons.forEach(function(value) {
   calculator.appendChild(button);
 });
 
+function found(arr, elem) {
+  return arr.indexOf(elem) != -1;
+}
+
+function isOperator(char) {
+  return found(operators, char);
+}
+
+function isNumber(char) {
+  return found(numbers, char);
+}
+
 function buttonClick(value) {
   var formula = document.getElementById('display').textContent;
+  var lastCharPos = formula.length - 1;
+  var lastChar = formula.charAt(lastCharPos);
+  var penultimateChar = formula.charAt(lastCharPos - 1);
+
+  // If we pressed Clear...
   if (value === 'C') {
     display.innerHTML = initialMessage;
     return;
   }
-  // If we didn't press Clear...
-  if (formula === initialMessage)
-    display.innerHTML = value;
-  else
-    display.innerHTML = formula + value;
+
+  // Replace the last character if...
+  // it was an operator and this is also an operator e.g. + becomes -
+  // it was a zero not following a number e.g. 01 becomes 1
+  // it was a zero on its own
+  if ((isOperator(value) && isOperator(lastChar))
+    || (lastChar === '0' && !isNumber(penultimateChar))
+    || (lastChar === '0' && penultimateChar == ''))
+    {
+      formula = formula.substring(0, lastCharPos);
+    }
+
+  display.innerHTML = formula + value;
 }
