32c23e6728120a556bf72f4dc957462c8e0c552e
samblackmore <samblackmore07@gmail.com>
2017-03-02 19:39:19 -0500

    making buttons

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-document.write("hello!");
+var buttons = [];
+
+// ASCII codes for all the buttons on our calculator
+// plus a pesky comma
+for (var i = 40; i <= 57; i++) {
+    buttons.push(String.fromCharCode(i));
+}
+buttons.splice(buttons.indexOf(','), 1);
+
+buttons.forEach(function(button) {
+  document.write('<button>' + button + '</button>');
+});
