0f2248736a087b09648b1a89ab768f3a72b36feb
samblackmore <samblackmore07@gmail.com>
2017-03-02 20:27:25 -0500

    adding style to look vaguely calculator-like

--- main.js
+++ main.js
@@ -6,13 +6,15 @@ for (var i = 40; i <= 57; i++) {
     buttons.push(String.fromCharCode(i));
 }
 buttons.splice(buttons.indexOf(','), 1);
+buttons = buttons.reverse();
 
 buttons.forEach(function(value) {
+  var calculator = document.getElementById('calculator');
   var button = document.createElement('button');
   var text = document.createTextNode(value);
   button.appendChild(text);
   button.setAttribute('onclick', 'buttonClick("' + value + '")');
-  document.body.appendChild(button);
+  calculator.appendChild(button);
 });
 
 function buttonClick(button) {
