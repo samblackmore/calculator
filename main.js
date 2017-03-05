364ca746ec72de82d465af4d1d179e7b9494a9d1
samblackmore <samblackmore07@gmail.com>
2017-03-05 17:25:46 -0500

    adding basic evaluation

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var numbers = [0,1,2,3,4,5,6,7,8,9].map(function(n) {return String(n)}).reverse();
-var operators = ['+','-','*','/'];
+var operators = ['*','/','+','-'];
 var brackets = ['(', ')'];
 var decimal = '.';
 var buttons = numbers.concat(operators).concat(brackets);
 buttons.push(decimal);
 buttons.push('C');
+buttons.push('=');
 
 buttons.forEach(function(value) {
   var calculator = document.getElementById('calculator');
@@ -19,12 +20,23 @@ function found(arr, elem) {
   return arr.indexOf(elem) != -1;
 }
 
+function isNumber(string) {
+  var chars = string.split('');
+  return chars.every(function(char) {
+    return found(numbers, s);
+  });
+}
+
 function isOperator(char) {
   return found(operators, char);
 }
 
-function isNumber(char) {
-  return found(numbers, char);
+function needToResolve(val) {
+  if (typeof val === 'number')
+    return false;
+  return operators.some(function(operator) {
+    return found(val, operator);
+  });
 }
 
 function stripLastChar(newChar, lastChar, penultimateChar) {
@@ -44,6 +56,53 @@ function stripLastChar(newChar, lastChar, penultimateChar) {
   return false;
 }
 
+function canOperate(formula, operator) {
+  // Check operator found in formula and formula can be split in 2
+  // TODO: Issue #3 - operator at start or end of formula should return false
+  return found(formula, operator) && formula.split(operator, 2).length === 2;
+}
+
+function resolver(formula, operator) {
+  var split = formula.split(operator, 2);
+  return evaluate(operator, split[0], split[1]);
+}
+
+function resolve(formula) {
+  if (!needToResolve(formula))
+    return formula;
+
+  console.log('Resolving ' + formula);
+
+  if (canOperate(formula, '*'))
+    return resolver(formula, '*');
+  else if (canOperate(formula, '/'))
+    return resolver(formula, '/');
+  else if (canOperate(formula, '+'))
+    return resolver(formula, '+');
+  else if (canOperate(formula, '-'))
+    return resolver(formula, '-');
+}
+
+function evaluate(operator, lhs, rhs) {
+  console.log('Evaluating (' + lhs + ') ' + operator + ' (' + rhs + ')');
+  if (!needToResolve(lhs) && !needToResolve(rhs)) {
+    var solution;
+    lhs = parseInt(lhs);
+    rhs = parseInt(rhs);
+    switch (operator) {
+      case '*': solution = lhs * rhs; break;
+      case '/': solution = lhs / rhs; break;
+      case '+': solution = lhs + rhs; break;
+      case '-': solution = lhs - rhs; break;
+    }
+    console.log('Solution ' + solution);
+    return solution;
+  }
+  var newLHS = resolve(lhs);
+  var newRHS = resolve(rhs);
+  return evaluate(operator, newLHS, newRHS);
+}
+
 function buttonClick(value) {
   var formula = document.getElementById('display').textContent;
   var lastCharPos = formula.length - 1;
@@ -53,6 +112,11 @@ function buttonClick(value) {
   // If we pressed Clear...
   if (value === 'C') {
     display.innerHTML = 0;
+    return;
+  }
+
+  if (value === '=') {
+    display.innerHTML = resolve(formula);
     return;
   }
 
