82165a69c935fb1498a34ce4013f30dd06db215a
samblackmore <samblackmore07@gmail.com>
2017-03-08 21:47:49 -0500

    putting vars on each line to avoid getting stung by an unseen semicolon

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Globals
-var cConsoleDim = '#afcfaf',
-    numbers = [0,1,2,3,4,5,6,7,8,9].map(function(n) {return String(n)}).reverse(),
-    operators = ['*','/','+','-'],
-    brackets = ['(', ')'],
-    decimal = '.',
-    solved = false,
-    buttons = numbers.concat(operators).concat(brackets).concat(decimal).concat('C').concat('='),
-    cons = document.getElementById('console'),
-    display = document.getElementById('display');
+var cConsoleDim = '#afcfaf';
+var numbers = [0,1,2,3,4,5,6,7,8,9].map(function(n) {return String(n)}).reverse();
+var operators = ['*','/','+','-'];
+var brackets = ['(', ')'];
+var decimal = '.';
+var solved = false;
+var buttons = numbers.concat(operators).concat(brackets).concat(decimal).concat('C').concat('=');
+var cons = document.getElementById('console');
+var display = document.getElementById('display');
 
 // Add buttons to calculator
 buttons.forEach(function(value) {
