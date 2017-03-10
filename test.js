048df9d0b8d36657a5d27c47eb36d6006727b34d
samblackmore <samblackmore07@gmail.com>
2017-03-10 09:10:41 -0500

    tests for +-*/

--- test.js
+++ test.js
@@ -28,7 +28,28 @@ var solve = require('./math');
 
 var assert = require('assert');
   describe('#solve()', function() {
-    it('should be able to add values', function() {
-      assert.equal(3, solve('1+2'));
+    it('should do addition', function() {
+      assert.equal(3, solve('1+2'));                // Simple test
+      assert.equal(-2, solve('-5+3'));              // Negative
+      assert.equal(5000, solve('2000+3000'));       // Big numbers
+      assert.equal(0.011, solve('0.001+0.01'));     // Floats
+    });
+    it('should do subtraction', function() {
+      assert.equal(4, solve('6-2'));                // Simple test
+      assert.equal(-11, solve('-5-6'));             // Negative
+      assert.equal(-10000, solve('-6000-4000'));    // Big numbers
+      assert.equal(0.009, solve('0.01-0.001'));     // Floats
+    });
+    it('should do multiplication', function() {
+      assert.equal(15, solve('3*5'));               // Simple test
+      assert.equal(-8, solve('-2*4'));              // Negative
+      assert.equal(20000000, solve('4000*5000'));   // Big numbers
+      assert.equal(0.0025, solve('0.05*0.05'));     // Floats
+    });
+    it('should do division', function() {
+      assert.equal(3, solve('12/4'));               // Simple test
+      assert.equal(-5, solve('-10/2'));             // Negative
+      assert.equal(4, solve('20000/5000'));         // Big numbers
+      assert.equal(1.5, solve('0.03/0.02'));        // Floats
     });
 });
